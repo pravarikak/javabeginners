386feb16ee9fff322c7ef00e9ab002b9c9da3bd3
prav <prav@gmail.com>
2025-10-04 13:19:14 -0500

    Identifers, constants and variables

---
+++ src/3_JavaConstructs/constants.java
@@ -0,0 +1,14 @@
+Constant
+The value of the variable cannot be changed
+
+In c/c++ - const is used 
+
+Even though we have const keyword in java, We use a modifier "final"
+
+final double PI = 3.14;
+
+****Java recommendation- The variable name of the constant should be in upper case
+
+final double MAX_AGE = 65.6;
+
+final String name = "prav";
