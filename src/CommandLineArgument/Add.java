bf3a082183b01650a197b6b81ddf89eddb1581a0
prav <prav@gmail.com>
2025-10-03 22:03:36 -0500

    Command line argument

---
+++ src/CommandLineArgument/Add.java
@@ -0,0 +1,11 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println(args[0]); //123
+     System.out.println(args[1]); //321
+     System.out.println(args[0]+args[1]); //123321 since it is string
+	}
+
+}
