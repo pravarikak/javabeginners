21c6467c79f5665481e1b7f02f12fd4b0958e1fb
prav <prav@gmail.com>
2025-10-04 14:07:03 -0500

    datatypes

---
+++ src/3_JavaConstructs/datatypes.java
@@ -0,0 +1,34 @@
+In java we have 8 primitive datatypes and reference datatypes(For example String class, you created Student class that is also reference types)
+
+Integer types- 4
+
+byte - size is 1 byte(8 bits). Total - 2 power 8 = 256. So Range:-128 to +127. Out of 8 bites, one bite is for sign(+,-). so left with 2 power 7= 128
+default value = 0 
+short - size is 2 byte(16 bits). Total - 2 power 16. So Range: -32768 to 32767, one bite is for sign(+,-). so left with 2 power 15= 32768
+default value = 0 
+int - size is 4 byte(32 bits). Total - 2 power 32. So Range: -2.1B to +2.1B, one bite is for sign(+,-). so left with 2 power 31= 2.1B
+default value = 0 
+long - size is 8 byte(64 bits).bite 2 power 64. Range: -4000T, +4000T
+default value = 0L
+
+default type is int 
+
+
+Float types - 2
+
+Float- size: 4 bytes(32 bits) .7 precision
+default value = 0.0F
+double - size: 8 bytes(64 bits) .15 precision
+default value = 0.0
+
+default type is double
+
+Character type - 1
+
+char - size: 2 bytes(16 bits)
+
+default value = '\u0000'
+
+Boolean type - 1
+
+boolean - true, false
